b0049b0c4e3656cc3b0b642a3e2ac830dbf1ad68
Robin <314941525@qq.com>
2020-03-11 12:20:02 +0800

    Signed-off-by: Robin <314941525@qq.com> submit

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ import Json from './Json' //测试用数据
  *  所有测试用数据均存放于根目录json.js
  *  
  *  css部分使用了App.vue下的全局样式和iconfont图标，有需要图标库的可以留言。
- *  示例使用了uni.scss下的变量, 除变量外已尽量移除特有语法,可直接替换为其他预处理器使 用
+ *  示例使用了uni.scss下的变量, 除变量外已尽量移除特有语法,可直接替换为其他预处理器使用
  */
 const msg = (title, duration=1500, mask=false, icon='none')=>{
 	//统一提示方便全局修改
